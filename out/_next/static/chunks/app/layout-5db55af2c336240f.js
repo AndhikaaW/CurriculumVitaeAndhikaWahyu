932698abd4c3698a91cde3e7020c01bb43c71fef
AndhikaW <andhikaw61@gmail.com>
2025-07-05 00:41:10 +0700

    add

---
+++ out/_next/static/chunks/app/layout-5db55af2c336240f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4157:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9554,23))},9554:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[147,946,956,358],()=>s(4157)),_N_E=e.O()}]);
