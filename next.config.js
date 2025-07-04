be68e8e2846bf3f9739a960c5e3548f450864d17
AndhikaW <andhikaw61@gmail.com>
2025-07-05 00:01:29 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/CurriculumVitaeAndhikaWahyu', // GANTI dengan nama repo GitHub kamu
-  assetPrefix: '/CurriculumVitaeAndhikaWahyu',
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/CurriculumVitaeAndhikaWahyu', // GANTI dengan nama repo GitHub kamu
+//   assetPrefix: '/CurriculumVitaeAndhikaWahyu/app',
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
